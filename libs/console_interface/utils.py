737deaac5933e6719cb5b2f77636ed9115c6b702
Mathieu Walravens <m.walravens@students.ephec.be>
2020-11-10 22:37:50 +0100

    Improve print_underline

--- libs/console_interface/utils.py
+++ libs/console_interface/utils.py
@@ -4,7 +4,15 @@ import getpass
 getuser = getpass.getuser
 
 
-def print_underline(*args, style='-'):
-	txt = ' '.join(map(str, args)).rstrip()
-	print(txt)
-	print(style * math.ceil(len(txt) / len(style)))
+def print_underline(*args, style='-', **kwargs):
+	"""Prints an underlined message to the console.
+
+	:param args: Anything you want to display to the console.
+	:param style: The underline style.
+	:param kwargs: Optionnals given to the function "print".
+	"""
+	sep = kwargs.pop('sep', ' ')
+	txt = sep.join(map(str, args))
+	length = max(len(line.rstrip()) for line in txt.splitlines())
+
+	print(txt, style * math.ceil(length / len(style)), sep='\n', **kwargs)
