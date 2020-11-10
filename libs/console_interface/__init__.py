759133788f2b7e5924a1a011bbbc7649234efd68
Mathieu Walravens <m.walravens@students.ephec.be>
2020-11-10 19:15:54 +0100

    Ajout d'un titre dans une playlist

--- libs/console_interface/__init__.py
+++ libs/console_interface/__init__.py
@@ -31,8 +31,23 @@ def add_song(db: DBMuziek):
     return True
 
 
-    return True
 def add_song_playlist(db: DBMuziek, name: str, songs: list[str]):
+    playlist = db.execute('SELECT playlist_id, author FROM playlists WHERE name = ?', (name,)).fetchone()
+    if playlist is None:
+        print(f'The playlist "{name}" does not exists.')
+        return
+
+    playlist_id, author = playlist
+    for song_name in songs:
+        song = db.execute('SELECT song_id FROM songs WHERE name = ?', (song_name,)).fetchone()
+        if song is None:
+            print(f'The song "{song_name}" does not exists. Ignoring ...')
+            continue
+
+        db.execute('INSERT OR IGNORE INTO playlistSongs VALUES (?, ?)', (playlist_id, song[0]))
+        print(f'The song "{song_name}" has been successfully added to the playlist "{name}".')
+
+    db.commit()
 
 
 def add_group(db: DBMuziek):
