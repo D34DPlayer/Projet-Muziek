5ed4aed2d48bcd21863824bfca8e9f5cb8f9f40a
Mathieu Walravens <m.walravens@students.ephec.be>
2020-11-10 22:44:31 +0100

    Create playlist
    Creates the playlist if it does not already exist.

--- libs/console_interface/__init__.py
+++ libs/console_interface/__init__.py
@@ -37,8 +37,8 @@ def add_song(db: DBMuziek):
 def add_song_playlist(db: DBMuziek, name: str, songs: list[str]):
     playlist = db.execute('SELECT playlist_id, author FROM playlists WHERE name = ?', (name,)).fetchone()
     if playlist is None:
-        print(f'The playlist "{name}" does not exists.')
-        return
+        playlist = create_playlist(db, name)
+        print(f'The playlist "{name}" has been successfully created.')
 
     playlist_id, author = playlist
     for song_name in songs:
@@ -76,8 +76,9 @@ def list_album(db: DBMuziek, name: str):
 def list_playlist(db: DBMuziek, name: str):
     playlist = db.execute('SELECT playlist_id, author FROM playlists WHERE name = ?', (name,)).fetchone()
     if playlist is None:
-        print(f'The playlist "{name}" does not exists.')
-        return
+        playlist = create_playlist(db, name)
+        print(f'The playlist "{name}" has been successfully created.')
+        db.commit()
 
     playlist_id, author = playlist
     utils.print_underline(f'Playlist "{name}" by [{author}] :', style='=')
@@ -102,3 +103,16 @@ def list_playlist(db: DBMuziek, name: str):
             duration = '{}:{}'.format(*divmod(duration, 60))
 
         print(f'{i:>{length}}. ({duration}) {song} - {group}')
+
+
+def create_playlist(db: DBMuziek, name: str) -> (int, str):
+    """Create a playlist in the database and return its id and author.
+    Does not commit the transaction.
+
+    :param db: The to insert the new playlist.
+    :param name: The playlist's name.
+    :return: The playlist's id and its author.
+    """
+    author = utils.getuser()
+    cursor = db.execute('INSERT INTO playlists(name, author) VALUES (?, ?)', (name, author))
+    return cursor.lastrowid, author
