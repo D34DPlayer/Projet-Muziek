7b7ddd4839d04f3bd61476a61ccc5aeec361ea68
Mathieu Walravens <m.walravens@students.ephec.be>
2020-11-13 12:35:37 +0100

    Make flake8 happy

--- libs/console_interface/utils.py
+++ libs/console_interface/utils.py
@@ -5,14 +5,14 @@ getuser = getpass.getuser
 
 
 def print_underline(*args, style='-', **kwargs):
-	"""Prints an underlined message to the console.
+    """Prints an underlined message to the console.
 
-	:param args: Anything you want to display to the console.
-	:param style: The underline style.
-	:param kwargs: Optionnals given to the function "print".
-	"""
-	sep = kwargs.pop('sep', ' ')
-	txt = sep.join(map(str, args))
-	length = max(len(line.rstrip()) for line in txt.splitlines())
+    :param args: Anything you want to display to the console.
+    :param style: The underline style.
+    :param kwargs: Optionnals given to the function "print".
+    """
+    sep = kwargs.pop('sep', ' ')
+    txt = sep.join(map(str, args))
+    length = max(len(line.rstrip()) for line in txt.splitlines())
 
-	print(txt, style * math.ceil(length / len(style)), sep='\n', **kwargs)
+    print(txt, style * math.ceil(length / len(style)), sep='\n', **kwargs)
