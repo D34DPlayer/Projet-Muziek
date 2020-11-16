ce85cedd5b36d5691d2b095b3de27eb354645861
Mathieu Walravens <m.walravens@students.ephec.be>
2020-11-16 10:27:46 +0100

    Add project description

---
+++ libs/__version__.py
@@ -0,0 +1,8 @@
+__authors__ = [
+    'D34DPlayer',
+    'M-Walravens'
+]
+__description__ = 'Keep your music organized.'
+__title__ = 'muziek'
+__url__ = 'https://github.com/D34DPlayer/Projet-Muziek'
+__version__ = '0.1.0'
