0a449fcc6ba99171e0f57061a45f6b1c63867b86
Mathieu Walravens <m.walravens@students.ephec.be>
2020-12-06 22:53:56 +0100

    Move Token

--- libs/youtube_api/__init__.py
+++ libs/youtube_api/__init__.py
@@ -12,27 +12,6 @@ logger = logging.getLogger('youtube-api')
 URL_PLAYLISTS = 'https://www.googleapis.com/youtube/v3/playlists'
 
 
-class Token:
-    def __init__(self, db: DBMuziek):
-        self._database = db
-        self._oauth_code = db.get_setting('yt.oauth2.code')
-        self._refresh_token = db.get_setting('yt.oauth2.refresh')
-        self._scope = db.get_setting('yt.oauth2.scope', '').split(',')
-        self._expires_in = float(db.get_setting('yt.oauth2.expires_in', 0))
-        self._expires_at = float(db.get_setting('yt.oauth2.expires_at', 0))
-
-    def prompt_access(self, modify=False):
-    def needs_prompt(self) -> bool:
-        return self._oauth_code is None
-
-        return self.needs_prompt \
-            or self._access_token is None \
-            or time.time() > self._expires_at - self._expires_in / 2
-    @property
-    def can_read(self) -> bool:
-        return SCOPE_MODIFY in self._scope
-
-
 class YoutubeAPI:
     def __init__(self, db: DBMuziek):
         self._token = Token(db)
