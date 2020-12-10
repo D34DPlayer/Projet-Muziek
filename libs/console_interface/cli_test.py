273f88643b2e22fa0d37b78b5277b17e5458ef7f
D34DPlayer <d34dplayer@protonmail.com>
2020-12-10 21:28:12 +0100

    pep8

--- libs/console_interface/cli_test.py
+++ libs/console_interface/cli_test.py
@@ -1,4 +1,4 @@
-from .utils import *
+from . import utils as u
 
 
 def check_print(capsys, text):
@@ -15,16 +15,16 @@ def set_input(monkeypatch, text):
 
 # TEST UTILS
 def test_print_underline(capsys):
-    print_underline("test", style="=")
+    u.print_underline("test", style="=")
     check_print(capsys, "test\n====\n")
 
-    print_underline("test", "alt", style="=")
+    u.print_underline("test", "alt", style="=")
     check_print(capsys, "test alt\n========\n")
 
-    print_underline("Test")
+    u.print_underline("Test")
     check_print(capsys, "Test\n----\n")
 
-    print_underline("Abc", "cdb", sep=",", style="=")
+    u.print_underline("Abc", "cdb", sep=",", style="=")
     check_print(capsys, "Abc,cdb\n=======\n")
 
 
@@ -45,47 +45,47 @@ def test_display_songs(capsys):
         }
     ]
 
-    display_songs([])
+    u.display_songs([])
     check_print(capsys, "<empty>\n")
 
-    display_songs(songs[1:])
+    u.display_songs(songs[1:])
     check_print(capsys, "3. TestSong (0:30) - TestGroup2\n")
 
-    display_songs(songs)
+    u.display_songs(songs)
     check_print(capsys, "1. TestSong (1:30) - TestGroup (ft. ton, chien)\n3. TestSong (0:30) - TestGroup2\n")
 
 
 def test_question(monkeypatch, capsys):
     set_input(monkeypatch, "")
-    assert question("Test", "default") == "default"
+    assert u.question("Test", "default") == "default"
     check_print(capsys, "Test [default]:\n")
 
     set_input(monkeypatch, "ID")
-    assert question("TeSt") == "ID"
+    assert u.question("TeSt") == "ID"
     check_print(capsys, "TeSt:\n")
 
 
 def test_question_choice(monkeypatch, capsys):
-    assert question_choice("Oy", []) is None
+    assert u.question_choice("Oy", []) is None
 
     set_input(monkeypatch, "y")
-    assert question_choice("TeSt", ["y", "n"]) == "y"
+    assert u.question_choice("TeSt", ["y", "n"]) == "y"
     check_print(capsys, "TeSt [y/n]:\n")
 
 
 def test_format_duration():
-    assert format_duration(59) == "0:59"
-    assert format_duration(326) == "5:26"
-    assert format_duration(None) == "??:??"
+    assert u.format_duration(59) == "0:59"
+    assert u.format_duration(326) == "5:26"
+    assert u.format_duration(None) == "??:??"
 
 
 def test_pagination(monkeypatch, capsys):
     set_input(monkeypatch, '4')
-    assert pagination(20, 8) == 4
+    assert u.pagination(20, 8) == 4
     check_print(capsys, "Pages: 1 2 3 .. 6 7 [8] 9 10 .. 18 19 20\nDisplay another page: \n")
 
     set_input(monkeypatch, '3')
-    assert pagination(22, 22) == 3
+    assert u.pagination(22, 22) == 3
     check_print(capsys, "Pages: 1 2 3 .. 20 21 [22]\nDisplay another page: \n")
 
 
@@ -97,11 +97,11 @@ def test_choose(monkeypatch, capsys):
     }]
 
     set_input(monkeypatch, '2')
-    assert choose_song(query) is query[1]
+    assert u.choose_song(query) is query[1]
     check_print(capsys, "2 songs were found with that name from the groups:\n 1 : Joe\n 2 : Mama\nChoose one [1/2]:\n")
 
     set_input(monkeypatch, '1')
-    assert choose_album(query) is query[0]
+    assert u.choose_album(query) is query[0]
     check_print(capsys, "2 albums were found with that name from the groups:\n 1 : Joe\n 2 : Mama\nChoose one [1/2]:\n")
 
 
@@ -111,8 +111,8 @@ def test_encoding():
         "groups": [{"group_name": "TestGroup"}]
     }
 
-    encoded = encode(data)
-    decoded = decode(encoded)
+    encoded = u.encode(data)
+    decoded = u.decode(encoded)
 
     assert data is not decoded
     assert data == decoded
