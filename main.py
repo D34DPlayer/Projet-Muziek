63d034300bbaeb9b0ea38aab769c3760ed3a199b
Mathieu Walravens <m.walravens@students.ephec.be>
2020-12-14 12:38:27 +0100

    Supress traceback on KeyboardInterrput

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ Options:
   -h --help             Show this screen.
   -d --database <PATH>  Path to the local storage [default: muziek.db].
   -s --song <song>      Song(s) to add to the playlist.
-  -D --download         Donwload all the songs included in the playlist.
+  -D --download         Download all the songs included in the playlist.
   -e --export           Exports the playlist.
   -i --import           Imports a playlist with the provided name.
   -g --genre <genre>    Filter the songs listed based on the genre.
@@ -42,52 +42,56 @@ if __name__ == "__main__":
     filters = {k.lstrip('-'): v for k, v in args.items() if k.startswith('--')}
 
     with DBMuziek(args['--database']) as db:
-        if args['add']:
-            if args['song']:
-                cli.add_song(db)
-            elif args['group']:
-                cli.add_group(db)
-            elif args['album']:
-                cli.add_album(db)
+        try:
+            if args['add']:
+                if args['song']:
+                    cli.add_song(db)
+                elif args['group']:
+                    cli.add_group(db)
+                elif args['album']:
+                    cli.add_album(db)
 
-        elif args['youtube']:  # overwrite `list` command
-            if args['list']:
-                cli.list_yt_playlist(db, args['<name>'])
-            elif args['import']:
-                cli.import_from_yt(db, args['<name>'])
-            elif args['export']:
-                cli.export_to_yt(db, args['<name>'])
+            elif args['youtube']:  # overwrite `list` command
+                if args['list']:
+                    cli.list_yt_playlist(db, args['<name>'])
+                elif args['import']:
+                    cli.import_from_yt(db, args['<name>'])
+                elif args['export']:
+                    cli.export_to_yt(db, args['<name>'])
 
-        elif args['list']:
-            if args['songs']:
-                cli.list_songs(db, filters)
-            elif args['group']:
-                cli.list_group(db, args['<name>'])
-            elif args['album']:
-                cli.list_album(db, args['<name>'])
-            elif args['playlists']:
-                cli.list_playlists(db)
-            elif args['albums']:
-                cli.list_albums(db)
-            elif args['groups']:
-                cli.list_groups(db)
+            elif args['list']:
+                if args['songs']:
+                    cli.list_songs(db, filters)
+                elif args['group']:
+                    cli.list_group(db, args['<name>'])
+                elif args['album']:
+                    cli.list_album(db, args['<name>'])
+                elif args['playlists']:
+                    cli.list_playlists(db)
+                elif args['albums']:
+                    cli.list_albums(db)
+                elif args['groups']:
+                    cli.list_groups(db)
 
-        elif args['playlist']:
-            if args['--download']:
-                cli.download_playlist(db, args["<name>"])
-            if args['--import']:
-                cli.import_playlist(db, args['<name>'])
-            elif args['--export']:
-                cli.export_playlist(db, args['<name>'])
-            elif len(args['--song']) == 0:
-                cli.list_playlist(db, args['<name>'])
+            elif args['playlist']:
+                if args['--download']:
+                    cli.download_playlist(db, args["<name>"])
+                if args['--import']:
+                    cli.import_playlist(db, args['<name>'])
+                elif args['--export']:
+                    cli.export_playlist(db, args['<name>'])
+                elif len(args['--song']) == 0:
+                    cli.list_playlist(db, args['<name>'])
+                else:
+                    cli.add_song_playlist(db, args['<name>'], args['--song'])
+
+            elif args['download']:
+                if args['song']:
+                    cli.download_song(db, args['<name>'])
+
             else:
-                cli.add_song_playlist(db, args['<name>'], args['--song'])
+                from libs import graphical_interface as gui
+                gui.run(db)
 
-        elif args['download']:
-            if args['song']:
-                cli.download_song(db, args['<name>'])
-
-        else:
-            from libs import graphical_interface as gui
-            gui.run(db)
+        except KeyboardInterrupt:
+            print('Exiting...')
