060cf7599798a0d5ccd88389f909aa9ef4771264
D34DPlayer <d34dplayer@protonmail.com>
2020-12-15 16:53:03 +0100

    GUI: utuils InfoPopup

--- libs/graphical_interface/utils.py
+++ libs/graphical_interface/utils.py
@@ -11,3 +11,12 @@ class ErrorPopup(Popup):
         self.ids.text_label.text = error
         self.title = "ERROR"
         self.open()
+
+
+class InfoPopup(Popup):
+    def __init__(self, error, **kwargs):
+        super(InfoPopup, self).__init__(**kwargs)
+
+        self.ids.text_label.text = error
+        self.title = "INFO"
+        self.open()
